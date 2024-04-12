bb68668e6a33e2d1887affffe5e726a780bf1c88
Kunth Shah <kunth.r.shah@gmail.com>
2024-04-12 17:16:22 +0530

    Implemented Color Recognizer
    Color recognizer code was fixed and updated.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ int main() {
     string filename = "car_prices.csv";
     vector<Car> cars = readDataFromFile(filename);
 
-    cout << "Enter the header to sort the dataset (YEAR, MAKE, MODEL, TRIM, BODY, TRANSMISSION, VIN, STATE, CONDITION, ODOMETER, COLOR, INTERIOR, SELLER, MMR, SELLINGPRICE, SALEDATE): ";
+    // General Sorting testing
+    /* cout << "Enter the header to sort the dataset (YEAR, MAKE, MODEL, TRIM, BODY, TRANSMISSION, VIN, STATE, CONDITION, ODOMETER, COLOR, INTERIOR, SELLER, MMR, SELLINGPRICE, SALEDATE): ";
     string header;
     cin >> header;
 
@@ -21,19 +22,22 @@ int main() {
         mergeSortInteger(cars, header, 0, cars.size() - 1);
     } else {
         cars = mergeSortString(cars, header);
-    }
-// ?
+    } */
+
     // cout << "\nFirst 500 cars after sorting:\n";
     // printFirst500Cars(cars);
 
+
     // Calculate and display average selling price by year
+    /* 
     cout << "\nAverage Selling Price by Year:\n";
     auto averageMap = calculateAverageByYear(cars);
     for (const auto& entry : averageMap) {
         cout << "Year: " << entry.first << ", Average Selling Price: " << entry.second.first << ", Number of Cars: " << entry.second.second << endl;
-    }
+    } */
 
-    Color_recognizer(cars);
+    // Color Recognizer
+    // Color_recognizer(cars);
 
     return 0;
 }
